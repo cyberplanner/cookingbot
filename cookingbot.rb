863104662c1daaa0ed674e9511745799e11ce84b
cyberplanner <sal@salweb.co.uk>
2016-11-24 15:35:50 +0000

    :tada: Bot is up and running.

--- cookingbot.rb
+++ cookingbot.rb
@@ -1,5 +1,17 @@
-#!/usr/bin/env/ ruby
-require 'dotenv-rails'
+#!/usr/bin/env ruby
+require 'dotenv'
 require 'telegram/bot'
+Dotenv.load
+token = ENV['TOKEN']
 
-token = ENV["TOKEN"]
+puts "It's alive!"
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+     bot.api.send_message(chat_id: message.chat.id, text: "Hi, I'm the cookingbot, how can I help you?")
+   when '/recipes'
+      bot.api.send_message(chat_id: message.chat.id, text: "What are your ingredients?")
+    end
+  end
+end
